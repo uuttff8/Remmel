cedb905db34b8f0b3a040effbfad79b40ef3322d
Anton Kuzmin <sassanid18@gmail.com>
2020-09-11 23:26:56 +0300

    prepare table view

--- Lemmy-iOS/Features/FrontPage/FrontPageViewController.swift
+++ Lemmy-iOS/Features/FrontPage/FrontPageViewController.swift
@@ -7,11 +7,64 @@
 //
 
 import UIKit
+import SnapKit
 
 class FrontPageViewController: UIViewController {
-
+    
+    let tableView = UITableView()
+    let searchController = UISearchController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor.red
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView()
+        
+        self.view.addSubview(tableView)
+        
+        tableView.snp.makeConstraints { (make) in
+            make.edges.equalToSuperview()
+        }
+        
+        navigationItem.searchController = searchController
+        navigationItem.title = "Lemmy"
+        self.view.backgroundColor = UIColor.systemBackground
     }
+}
+
+
+extension FrontPageViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return FrontPageCells.allCases.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let section = FrontPageCells.allCases[section]
+        switch section {
+        case .header:
+            return 1
+        case .content:
+            return 3
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let section = FrontPageCells.allCases[indexPath.section]
+        
+        switch section {
+        case .header:
+            return UITableViewCell()
+        case .content:
+            let cell = UITableViewCell()
+            cell.backgroundColor = .red
+            return cell
+        }
+    }
+}
+
+
+private enum FrontPageCells: CaseIterable {
+    case header
+    case content
 }
