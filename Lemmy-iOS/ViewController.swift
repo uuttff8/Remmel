b067e64692f1db09c209acf105412121037115c6
Anton Kuzmin <sassanid18@gmail.com>
2020-09-11 13:39:44 +0300

    Initial Commit

---
+++ Lemmy-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lemmy-iOS
+//
+//  Created by uuttff8 on 9/11/20.
+//  Copyright © 2020 Anton Kuzmin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Lemmy-iOSTests/Lemmy_iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  Lemmy_iOSTests.swift
+//  Lemmy-iOSTests
+//
+//  Created by uuttff8 on 9/11/20.
+//  Copyright © 2020 Anton Kuzmin. All rights reserved.
+//
+
+import XCTest
+@testable import Lemmy_iOS
+
+class Lemmy_iOSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
