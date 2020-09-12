cba12d4c812c9872bb54987d3589178fcfbaf728
Anton Kuzmin <sassanid18@gmail.com>
2020-09-12 16:50:55 +0300

    refactor mvc

--- Lemmy-iOS/Features/FrontPage/FrontPageModel.swift
+++ Lemmy-iOS/Features/FrontPage/FrontPageModel.swift
@@ -8,6 +8,74 @@
 
 import UIKit
 
-class FrontPageModel {
+class FrontPageModel: NSObject {
+    var postsLoaded: (() -> Void)?
     
+    var postsDataSource: Array<LemmyApiStructs.PostView>?
+    
+    func loadPosts() {
+        let parameters = LemmyApiStructs.Post.GetPostsRequest(type_: "All",
+                                                              sort: "Active",
+                                                              page: 1,
+                                                              limit: 20,
+                                                              communityId: nil,
+                                                              communityName: nil,
+                                                              auth: nil)
+        
+        ApiManager.shared.requestsManager.getPosts(
+            parameters: parameters,
+            completion: { (dec: Result<LemmyApiStructs.Post.GetPostsResponse, Error>) in
+                switch dec {
+                case .success(let posts):
+                    self.postsDataSource = posts.posts
+                    DispatchQueue.main.async {
+                        self.postsLoaded?()
+                    }
+                case .failure(let error):
+                    print(error)
+                }
+        })
+    }
+}
+
+
+extension FrontPageModel: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return FrontPageCells.allCases.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let section = FrontPageCells.allCases[section]
+        switch section {
+        case .header:
+            return 1
+        case .content:
+            guard let posts = postsDataSource else {
+                return 0
+            }
+            return posts.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let section = FrontPageCells.allCases[indexPath.section]
+        
+        switch section {
+        case .header:
+            return FrontPageHeaderCell()
+        case .content:
+            guard let posts = postsDataSource else {
+                return UITableViewCell()
+            }
+            let postCell = PostContentTableCell()
+            postCell.bind(with: posts[indexPath.row])
+            return postCell
+        }
+    }
+}
+
+
+private enum FrontPageCells: CaseIterable {
+    case header
+    case content
 }
--- Lemmy-iOS/Features/FrontPage/FrontPageViewController.swift
+++ Lemmy-iOS/Features/FrontPage/FrontPageViewController.swift
@@ -11,11 +11,15 @@ import SnapKit
 
 class FrontPageViewController: UIViewController {
     
+    let model = FrontPageModel()
+    
     let tableView: UITableView = {
         let tableView = UITableView()
         tableView.tableFooterView = UIView()
         tableView.rowHeight = UITableView.automaticDimension
         tableView.estimatedRowHeight = 40
+        tableView.keyboardDismissMode = .onDrag
+        tableView.separatorStyle = .none
         return tableView
     }()
     
@@ -30,83 +34,27 @@ class FrontPageViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.delegate = self
-        tableView.dataSource = self
+        self.view.backgroundColor = UIColor.systemBackground
+        
+        model.loadPosts()
+        model.postsLoaded = {
+            self.tableView.reloadData()
+        }
+    }
+    
+    private func setupTableView() {
+        tableView.delegate = model
+        tableView.dataSource = model
         
         self.view.addSubview(tableView)
         
-        tableView.keyboardDismissMode = .onDrag
-        tableView.separatorStyle = .none
         tableView.snp.makeConstraints { (make) in
             make.edges.equalToSuperview()
         }
+    }
+    
+    private func setupNavigationItem() {
         navigationItem.titleView = searchController.searchBar
         navigationItem.titleView?.frame.size.width = UIScreen.main.bounds.width
-        self.view.backgroundColor = UIColor.systemBackground
-        
-        
-        let parameters = LemmyApiStructs.Post.GetPostsRequest(type_: "All",
-                                                              sort: "Active",
-                                                              page: 1,
-                                                              limit: 20,
-                                                              communityId: nil,
-                                                              communityName: nil,
-                                                              auth: nil)
-        
-        ApiManager.shared.requestsManager.getPosts(
-            parameters: parameters,
-            completion: { (dec: Result<LemmyApiStructs.Post.GetPostsResponse, Error>) in
-                switch dec {
-                case .success(let sss):
-                    self.posts = sss.posts
-                    DispatchQueue.main.async {
-                        self.tableView.reloadData()
-                    }
-                case .failure(let error):
-                    print(error)
-                }
-        })
     }
-}
-
-
-extension FrontPageViewController: UITableViewDelegate, UITableViewDataSource {
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return FrontPageCells.allCases.count
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let section = FrontPageCells.allCases[section]
-        switch section {
-        case .header:
-            return 1
-        case .content:
-            guard let posts = posts else {
-                return 0
-            }
-            return posts.count
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let section = FrontPageCells.allCases[indexPath.section]
-        
-        switch section {
-        case .header:
-            return FrontPageHeaderCell()
-        case .content:
-            guard let posts = posts else {
-                return UITableViewCell()
-            }
-            let postCell = PostContentTableCell()
-            postCell.bind(with: posts[indexPath.row])
-            return postCell
-        }
-    }
-}
-
-
-private enum FrontPageCells: CaseIterable {
-    case header
-    case content
 }
