59dda3fdfc0567caba61cd77067a95f7ac53ac7c
Anton Kuzmin <sassanid18@gmail.com>
2020-09-16 15:32:46 +0300

    add navigation item

--- Lemmy-iOS/Features/Communities/CommunitiesViewController.swift
+++ Lemmy-iOS/Features/Communities/CommunitiesViewController.swift
@@ -25,6 +25,7 @@ class CommunitiesViewController: UIViewController {
     
     override func viewDidLoad() {
         self.view.backgroundColor = UIColor.systemBackground
+        self.navigationItem.title = "Communities"
         
         setupTableView()
         model.loadCommunities()
