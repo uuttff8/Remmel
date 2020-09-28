6992b8bf5e9e37e49f556e443b6ff76e3964d0b9
Anton Kuzmin <sassanid18@gmail.com>
2020-09-28 14:03:44 +0300

    create login or register button class

---
+++ Lemmy-iOS/Base/CustomViews/LoginOrRegisterButton.swift
@@ -0,0 +1,13 @@
+//
+//  LoginOrRegisterButton.swift
+//  Lemmy-iOS
+//
+//  Created by uuttff8 on 9/28/20.
+//  Copyright © 2020 Anton Kuzmin. All rights reserved.
+//
+
+import UIKit
+
+class LoginOrRegisterButton: UIButton {
+    
+}
