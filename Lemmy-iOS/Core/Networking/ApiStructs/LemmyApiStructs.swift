6a3d9676e0b62d5eecc0ed1295caccd41785bb10
Anton Kuzmin <sassanid18@gmail.com>
2020-10-14 19:01:24 +0300

    add SiteView struct

--- Lemmy-iOS/Core/Networking/ApiStructs/LemmyApiStructs.swift
+++ Lemmy-iOS/Core/Networking/ApiStructs/LemmyApiStructs.swift
@@ -32,7 +32,7 @@ enum LemmySortType: String, Codable, CaseIterable {
         case .week: return "Week"
         case .month: return "Month"
         case .all: return "All"
-//        case .topAll: return "TopAll"
+        //        case .topAll: return "TopAll"
         }
     }
     
@@ -45,7 +45,7 @@ enum LemmySortType: String, Codable, CaseIterable {
         case .week: return 4
         case .month: return 5
         case .all: return 6
-//        case .topAll: return 7
+        //        case .topAll: return 7
         }
     }
 }
@@ -100,83 +100,83 @@ enum LemmyApiStructs {
     
     // MARK: - PostView -
     struct PostView: Codable, Equatable {
-         let id: Int
-         let name: String
-         let url: String?
-         let body: String?
-         let communityId: Int
-         let removed: Bool
-         let locked: Bool
-         let published: String // Timestamp
-         let updated: String? // Timestamp
-         let deleted: Bool
-         let nsfw: Bool
-         let stickied: Bool
-         let embedTitle: String?
-         let embedDescription: String?
-         let embedHtml: String?
-         let thumbnailUrl: String?
-         let apId: String
-         let local: Bool
-         let creatorActorId: String
-         let creatorLocal: Bool
-         let creatorName: String
-         let creatorPreferredUsername: String?
-         let creatorPublished: String // Timestamp
-         let creatorAvatar: String?
-         let banned: Bool
-         let bannedFromCommunity: Bool
-         let communityActorId: String
-         let communityLocal: Bool
-         let communityName: String
-         let communityIcon: String?
-         let communityRemoved: Bool
-         let communityDeleted: Bool
-         let communityNsfw: Bool
-         let numberOfComments: Int
-         let score: Int
-         let upvotes: Int
-         let downvotes: Int
-         let hotRank: Int
-         let hotRankActive: Int
-         let newestActivityTime: String // Timestamp
-         let userId: Int?
-         let myVote: Int?
-         let subscribed: Bool?
-         let read: Bool?
-         let saved: Bool?
-         
-         enum CodingKeys: String, CodingKey {
-             case id, name, url, body
-             case communityId = "community_id"
-             case removed, locked, published, updated, deleted, nsfw, stickied
-             case embedTitle = "embed_title"
-             case embedDescription = "embed_description"
-             case embedHtml = "embed_html"
-             case thumbnailUrl = "thumbnail_url"
-             case apId = "ap_id", local
-             case creatorActorId = "creator_actor_id"
-             case creatorLocal = "creator_local"
-             case creatorName = "creator_name"
-             case creatorPreferredUsername = "creator_preferred_username"
-             case creatorPublished = "creator_published"
-             case creatorAvatar = "creator_avatar", banned
-             case bannedFromCommunity = "banned_from_community"
-             case communityActorId = "community_actor_id"
-             case communityLocal = "community_local"
-             case communityName = "community_name"
-             case communityIcon = "community_icon"
-             case communityRemoved = "community_removed"
-             case communityDeleted = "community_deleted"
-             case communityNsfw = "community_nsfw"
-             case numberOfComments = "number_of_comments"
-             case score, upvotes, downvotes
-             case hotRank = "hot_rank"
-             case hotRankActive = "hot_rank_active"
-             case newestActivityTime = "newest_activity_time"
-             case userId = "user_id"
-             case myVote = "my_vote", subscribed, read, saved
-         }
+        let id: Int
+        let name: String
+        let url: String?
+        let body: String?
+        let communityId: Int
+        let removed: Bool
+        let locked: Bool
+        let published: String // Timestamp
+        let updated: String? // Timestamp
+        let deleted: Bool
+        let nsfw: Bool
+        let stickied: Bool
+        let embedTitle: String?
+        let embedDescription: String?
+        let embedHtml: String?
+        let thumbnailUrl: String?
+        let apId: String
+        let local: Bool
+        let creatorActorId: String
+        let creatorLocal: Bool
+        let creatorName: String
+        let creatorPreferredUsername: String?
+        let creatorPublished: String // Timestamp
+        let creatorAvatar: String?
+        let banned: Bool
+        let bannedFromCommunity: Bool
+        let communityActorId: String
+        let communityLocal: Bool
+        let communityName: String
+        let communityIcon: String?
+        let communityRemoved: Bool
+        let communityDeleted: Bool
+        let communityNsfw: Bool
+        let numberOfComments: Int
+        let score: Int
+        let upvotes: Int
+        let downvotes: Int
+        let hotRank: Int
+        let hotRankActive: Int
+        let newestActivityTime: String // Timestamp
+        let userId: Int?
+        let myVote: Int?
+        let subscribed: Bool?
+        let read: Bool?
+        let saved: Bool?
+        
+        enum CodingKeys: String, CodingKey {
+            case id, name, url, body
+            case communityId = "community_id"
+            case removed, locked, published, updated, deleted, nsfw, stickied
+            case embedTitle = "embed_title"
+            case embedDescription = "embed_description"
+            case embedHtml = "embed_html"
+            case thumbnailUrl = "thumbnail_url"
+            case apId = "ap_id", local
+            case creatorActorId = "creator_actor_id"
+            case creatorLocal = "creator_local"
+            case creatorName = "creator_name"
+            case creatorPreferredUsername = "creator_preferred_username"
+            case creatorPublished = "creator_published"
+            case creatorAvatar = "creator_avatar", banned
+            case bannedFromCommunity = "banned_from_community"
+            case communityActorId = "community_actor_id"
+            case communityLocal = "community_local"
+            case communityName = "community_name"
+            case communityIcon = "community_icon"
+            case communityRemoved = "community_removed"
+            case communityDeleted = "community_deleted"
+            case communityNsfw = "community_nsfw"
+            case numberOfComments = "number_of_comments"
+            case score, upvotes, downvotes
+            case hotRank = "hot_rank"
+            case hotRankActive = "hot_rank_active"
+            case newestActivityTime = "newest_activity_time"
+            case userId = "user_id"
+            case myVote = "my_vote", subscribed, read, saved
+        }
         
         var postType: PostType {
             if self.url != nil {
@@ -189,7 +189,7 @@ enum LemmyApiStructs {
             
             return PostType.plainText
         }
-     }
+    }
     
     // MARK: - CommentView -
     struct CommentView: Codable, Equatable {
@@ -399,7 +399,7 @@ enum LemmyApiStructs {
         let publicKey: String?
         let lastRefreshedAt: String // Timestamp
         let banner: String?
-
+        
         enum CodingKeys: String, CodingKey {
             case id, name
             case preferredUsername = "preferred_username"
@@ -597,6 +597,45 @@ enum LemmyApiStructs {
             case recipientId = "recipient_id"
             case recipientActorId = "recipient_actor_id"
             case recipientLocal = "recipient_local"
+        }
+    }
+    
+    // MARK: - SiteView -
+    struct SiteView: Codable, Equatable {
+        let id: Int
+        let name: String
+        let description: String?
+        let creatorId: Int
+        let published: String // Timestamp
+        let updated: String? // Timestamp
+        let enableDownvotes: Bool
+        let openRegistration: Bool
+        let enableNsfw: Bool
+        let icon: String?
+        let banner: String?
+        let creatorName: String
+        let creatorPreferredUsername: String?
+        let creatorAvatar: String?
+        let numberOfUsers: Int
+        let numberOfPosts: Int
+        let numberOfComments: Int
+        let numberOfCommunities: Int
+        
+        enum CodingKeys: String, CodingKey {
+            case id, name, description
+            case creatorId = "creator_id"
+            case published, updated
+            case enableDownvotes = "enable_downvotes"
+            case openRegistration = "open_registration"
+            case enableNsfw = "enable_nsfw"
+            case icon, banner
+            case creatorName = "creator_name"
+            case creatorPreferredUsername = "creator_preferred_username"
+            case creatorAvatar = "creator_avatar"
+            case numberOfUsers = "number_of_users"
+            case numberOfPosts = "number_of_posts"
+            case numberOfComments = "number_of_comments"
+            case numberOfCommunities = "number_of_communities"
         }
     }
 }
