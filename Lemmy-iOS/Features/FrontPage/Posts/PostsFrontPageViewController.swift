4a01e68f9972d2e7285475fcfc9b99d14a865d48
Anton Kuzmin <sassanid18@gmail.com>
2020-10-16 23:19:36 +0300

    clearing

--- Lemmy-iOS/Features/FrontPage/Posts/PostsFrontPageViewController.swift
+++ Lemmy-iOS/Features/FrontPage/Posts/PostsFrontPageViewController.swift
@@ -26,7 +26,6 @@ class PostsFrontPageViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = model
-//        tableView.dataSource = model
         
         tableView.register(
             PostContentTableCell.self,
@@ -48,21 +47,6 @@ class PostsFrontPageViewController: UIViewController {
         model.newDataLoaded = { [self] newPosts in
             addRows(with: newPosts)
         }
-//
-//        model.newDataLoaded = { [self] (newPosts) in
-//            let startIndex = model.postsDataSource.count - newPosts.count
-//            let endIndex = startIndex + newPosts.count
-//
-//            let newIndexpaths =
-//                Array(startIndex ..< endIndex)
-//                .map { (index) in
-//                    IndexPath(row: index, section: 0)
-//                }
-//
-//            tableView.performBatchUpdates {
-//                tableView.insertRows(at: newIndexpaths, with: .automatic)
-//            }
-//        }
         
         model.goToPostScreen = { [self] (post: LemmyApiStructs.PostView) in
             coordinator?.goToPostScreen(post: post)
@@ -70,9 +54,6 @@ class PostsFrontPageViewController: UIViewController {
     }
     
     func addRows(with list: Array<LemmyApiStructs.PostView>, animate: Bool = true) {
-        
-        // Last item from 
-        let lastItem = (model.postsDataSource.count - 1) - (model.currentPage * 20 - 20)
         snapshot.insertItems(list, afterItem: model.postsDataSource.last!)
         self.model.postsDataSource.append(contentsOf: list)
         dataSource.apply(snapshot, animatingDifferences: false)
@@ -88,19 +69,9 @@ class PostsFrontPageViewController: UIViewController {
         return UITableViewDiffableDataSource<Section, LemmyApiStructs.PostView>(
             tableView: tableView,
             cellProvider: { (tableView, indexPath, postView) -> UITableViewCell? in
-//                let cell: UITableViewCell = { [self] in
-//                    guard let postCell =
-//                            tableView.dequeueReusableCell(withIdentifier: PostContentTableCell.reuseId) as? PostContentTableCell
-//                    else {
-//                        return UITableViewCell(style: .default, reuseIdentifier: "cell")
-//                    }
-                
                 let cell = PostContentTableCell()
                 cell.postContentView.delegate = self.model
                 cell.bind(with: self.model.postsDataSource[indexPath.row])
-
-//                    return postCell
-//                }()
                 
                 return cell
         })
