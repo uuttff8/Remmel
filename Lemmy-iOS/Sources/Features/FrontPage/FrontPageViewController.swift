1f8e245c70c68a444bb284998e8a46dfa84ca1ae
Anton Kuzmin <sassanid18@gmail.com>
2020-11-14 15:09:17 +0300

    crete view model for it

--- Lemmy-iOS/Sources/Features/FrontPage/FrontPageViewController.swift
+++ Lemmy-iOS/Sources/Features/FrontPage/FrontPageViewController.swift
@@ -9,40 +9,6 @@
 import UIKit
 import SnapKit
 
-protocol FrontPageViewModelProtocol {
-    func doNavBarProfileAction()
-}
-
-class FrontPageViewModel: FrontPageViewModelProtocol {
-    private let userAccountService: UserAccountSerivceProtocol
-    
-    weak var viewController: FrontPageViewControllerProtocol?
-    
-    init(
-        userAccountService: UserAccountSerivceProtocol
-    ) {
-        self.userAccountService = userAccountService
-    }
-    
-    func doNavBarProfileAction() {
-        if let user = self.userAccountService.currentUser {
-            self.viewController?.displayProfileScreen(viewModel: .init(user: user))
-        } else {
-            self.viewController?.displayAutorizationAlert()
-        }
-    }
-}
-
-enum FrontPage {
-    
-    enum ProfileAction {
-        
-        struct ViewModel {
-            let user: LemmyModel.MyUser
-        }
-    }
-}
-
 protocol FrontPageViewControllerProtocol: AnyObject {
     func displayAutorizationAlert()
     func displayProfileScreen(viewModel: FrontPage.ProfileAction.ViewModel)
---
+++ Lemmy-iOS/Sources/Features/FrontPage/FrontPageViewModel.swift
@@ -0,0 +1,43 @@
+//
+//  FrontPageViewModel.swift
+//  Lemmy-iOS
+//
+//  Created by uuttff8 on 14.11.2020.
+//  Copyright © 2020 Anton Kuzmin. All rights reserved.
+//
+
+import Foundation
+
+protocol FrontPageViewModelProtocol {
+    func doNavBarProfileAction()
+}
+
+class FrontPageViewModel: FrontPageViewModelProtocol {
+    private let userAccountService: UserAccountSerivceProtocol
+    
+    weak var viewController: FrontPageViewControllerProtocol?
+    
+    init(
+        userAccountService: UserAccountSerivceProtocol
+    ) {
+        self.userAccountService = userAccountService
+    }
+    
+    func doNavBarProfileAction() {
+        if let user = self.userAccountService.currentUser {
+            self.viewController?.displayProfileScreen(viewModel: .init(user: user))
+        } else {
+            self.viewController?.displayAutorizationAlert()
+        }
+    }
+}
+
+enum FrontPage {
+    
+    enum ProfileAction {
+        
+        struct ViewModel {
+            let user: LemmyModel.MyUser
+        }
+    }
+}
