88e32103accacfb4aaa88ea2b0965c9024caad2a
Anton Kuzmin <sassanid18@gmail.com>
2020-11-25 20:26:13 +0300

    delete comment node

--- Lemmy-iOS/Sources/Core/Common/Comments/CommentTypes.swift
+++ Lemmy-iOS/Sources/Core/Common/Comments/CommentTypes.swift
@@ -34,9 +34,3 @@ class BaseComment: AbstractComment {
         self.replies.append(reply)
     }
 }
-
-struct CommentNode {
-    let id: Int
-    let comment: LemmyModel.CommentView
-    var replies: [CommentNode]
-}
