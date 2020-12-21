4c790c09aa95a32788e595b6ee48cc82da7c20c3
Anton Kuzmin <sassanid18@gmail.com>
2020-12-21 03:29:14 +0300

    some good addings

--- Lemmy-iOS/Sources/Core/UIUtils/Then.swift
+++ Lemmy-iOS/Sources/Core/UIUtils/Then.swift
@@ -6,15 +6,20 @@
 //  Copyright © 2020 Anton Kuzmin. All rights reserved.
 //
 
-// https://github.com/brave/brave-ios/blob/v1.20/ThirdParty/Then.swift
+// https://github.com/brave/brave-ios/blob/development/ThirdParty/Then.swift
+// https://github.com/capnslipp/With/blob/master/Sources/With.swift
+
+// Use then { } function where available, if not then use with(Type()) {  }
 
 import Foundation
-import CoreGraphics
+#if !os(Linux)
+  import CoreGraphics
+#endif
 #if os(iOS) || os(tvOS)
   import UIKit.UIGeometry
 #endif
 
-protocol Then {}
+public protocol Then {}
 
 extension Then where Self: Any {
 
@@ -24,7 +29,8 @@ extension Then where Self: Any {
   ///       $0.origin.x = 10
   ///       $0.size.width = 100
   ///     }
-  func with(_ block: (inout Self) throws -> Void) rethrows -> Self {
+  @inlinable
+  public func with(_ block: (inout Self) throws -> Void) rethrows -> Self {
     var copy = self
     try block(&copy)
     return copy
@@ -37,7 +43,8 @@ extension Then where Self: Any {
   ///       $0.set("devxoul@gmail.com", forKey: "email")
   ///       $0.synchronize()
   ///     }
-  func `do`(_ block: (Self) throws -> Void) rethrows {
+  @inlinable
+  public func `do`(_ block: (Self) throws -> Void) rethrows {
     try block(self)
   }
 
@@ -48,11 +55,12 @@ extension Then where Self: AnyObject {
   /// Makes it available to set properties with closures just after initializing.
   ///
   ///     let label = UILabel().then {
-  ///       $0.textAlignment = .Center
-  ///       $0.appearanceTextColor = UIColor.blackColor()
+  ///       $0.textAlignment = .center
+  ///       $0.textColor = UIColor.black
   ///       $0.text = "Hello, World!"
   ///     }
-  func then(_ block: (Self) throws -> Void) rethrows -> Self {
+  @inlinable
+  public func then(_ block: (Self) throws -> Void) rethrows -> Self {
     try block(self)
     return self
   }
@@ -61,13 +69,45 @@ extension Then where Self: AnyObject {
 
 extension NSObject: Then {}
 
-extension CGPoint: Then {}
-extension CGRect: Then {}
-extension CGSize: Then {}
-extension CGVector: Then {}
+#if !os(Linux)
+  extension CGPoint: Then {}
+  extension CGRect: Then {}
+  extension CGSize: Then {}
+  extension CGVector: Then {}
+#endif
+
+extension Array: Then {}
+extension Dictionary: Then {}
+extension Set: Then {}
 
 #if os(iOS) || os(tvOS)
   extension UIEdgeInsets: Then {}
   extension UIOffset: Then {}
   extension UIRectEdge: Then {}
 #endif
+
+// MARK: Returning Subject
+/// “With” on an object- or value-type subject, returning the same subject
+/// (including any mutations performed in the closure).
+@inlinable @discardableResult
+public func with<SubjectT>(
+    _ subject: SubjectT,
+    _ operations: (inout SubjectT) throws -> Void
+) rethrows -> SubjectT {
+    var subject = subject
+    try operations(&subject)
+    return subject
+}
+
+// MARK: Returning Arbitrary Value
+/// “With” on an object- or value-type subject, returning an aribitrary return object/value from the closure
+///  (the subject is still mutated).
+@inlinable
+public func withMap<SubjectT, ReturnT>(
+    _ subject: SubjectT,
+    _ transform: (inout SubjectT) throws -> ReturnT
+) rethrows -> ReturnT {
+    var subject = subject
+    return try transform(&subject)
+}
+
