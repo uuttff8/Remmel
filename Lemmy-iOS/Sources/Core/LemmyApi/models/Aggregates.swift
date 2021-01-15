fb52f26c5c3e7f8fb0664bb36bc76ffbef9a3e57
Anton Kuzmin <sassanid18@gmail.com>
2021-01-15 19:00:48 +0300

    update version & create aggregates

---
+++ Lemmy-iOS/Sources/Core/LemmyApi/models/Aggregates.swift
@@ -0,0 +1,53 @@
+//
+//  Aggregates.swift
+//  Lemmy-iOS
+//
+//  Created by uuttff8 on 15.01.2021.
+//  Copyright © 2021 Anton Kuzmin. All rights reserved.
+//
+
+import Foundation
+
+struct UserAggregates {
+  let id: Int
+  let user_id: Int
+  let post_count: Int
+  let post_score: Int
+  let comment_count: Int
+  let comment_score: Int
+}
+
+struct SiteAggregates {
+  let id: Int
+  let site_id: Int
+  let users: Int
+  let posts: Int
+  let comments: Int
+  let communities: Int
+}
+
+struct PostAggregates {
+  let id: Int
+  let post_id: Int
+  let comments: Int
+  let score: Int
+  let upvotes: Int
+  let downvotes: Int
+  let newest_comment_time: Int
+}
+
+struct CommunityAggregates {
+  let id: Int
+  let community_id: Int
+  let subscribers: Int
+  let posts: Int
+  let comments: Int
+}
+
+struct CommentAggregates {
+  let id: Int
+  let comment_id: Int
+  let score: Int
+  let upvotes: Int
+  let downvotes: Int
+}
