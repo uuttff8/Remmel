a0f2ad7905b1cb25391c631ce25f8fa96a6f67a8
Anton Kuzmin <sassanid18@gmail.com>
2021-07-21 11:41:11 +0300

    update deps and add pods to gitignore

--- Pods/Target Support Files/DateToolsSwift/DateToolsSwift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_DateToolsSwift : NSObject
-@end
-@implementation PodsDummy_DateToolsSwift
-@end
--- Pods/Target Support Files/Pods-Lemmy-iOS/Pods-Lemmy-iOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Lemmy_iOS : NSObject
-@end
-@implementation PodsDummy_Pods_Lemmy_iOS
-@end
